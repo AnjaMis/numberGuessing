29b19d80fc6700c52a85363a533777e2a544854d
AnjaMis <bgp916@myy.haaga-helia.fi>
2022-03-16 10:09:15 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native'
 
 export default function App() {
+  const [number, setNumber] = React.useState(0)
+
+  const [guess, setGuess] = React.useState('')
+
+  const [text, setText] = React.useState('')
+
+  const [counter, setCounter] = React.useState(1)
+
+  React.useEffect(() => reset(), [])
+
+  const reset = () => {
+    setNumber(Math.floor(Math.random() * 100) + 1)
+    setGuess('')
+    setCounter(1)
+    setText('Guess a number between 0 and 100')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text style={{ fontSize: 18 }}>{text}</Text>
+        <TextInput
+          style={{ width: 200, borderColor: 'gray', borderWidth: 1 }}
+          value={guess}
+          onChangeText={(text) => setGuess(text)}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title=" MAKE GUESS "
+          onPress={() => {
+            if (parseInt(guess) === number) {
+              Alert.alert(
+                'You guessed the right number in ' + counter + ' guesses',
+              )
+              reset()
+            } else if (parseInt(guess) > number) {
+              setText('Your guess ' + guess + ' is too high')
+              setGuess('')
+            } else {
+              setText('Your guess ' + guess + ' is too low')
+              setGuess('')
+            }
+            setCounter((prevCounter) => prevCounter + 1)
+          }}
+        />
+        <StatusBar style="auto" />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  buttonContainer: {
+    flex: 5,
+    width: 80,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+  },
+})
